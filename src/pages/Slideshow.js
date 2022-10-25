66aebff85e5319a5bdc5dc04eba49658f32d1386
Gen <kebikovgen@gmail.com>
2022-10-25 12:38:58 +0300

    add page slides-show

--- src/pages/Slideshow.js
+++ src/pages/Slideshow.js
@@ -1,8 +1,36 @@
-import Spinner from "../components/Spiner/Spiner";
+import '../scss/slidesShow.scss';
+import DA from '../resources/video/D&A.mp4';
+import SL from '../resources/video/S&L.mp4';
+import MY from '../resources/video/M&Y.mp4';
+import NK from '../resources/video/N&K.mp4';
+import PT from '../resources/video/P&T.mp4';
+import SD from '../resources/video/S&D.mp4';
 
 const Slideshow = () => {
+
     return(
-        <Spinner/>
+        <div className="video">
+            <div className="video__body">
+                <div className="video__item">
+                    <video src={DA} controls/>
+                </div>
+                <div className="video__item">
+                    <video src={SL} controls/>
+                </div>
+                <div className="video__item">
+                    <video src={MY} controls/>
+                </div>
+                <div className="video__item">
+                    <video src={NK} controls/>
+                </div>
+                <div className="video__item">
+                    <video src={PT} controls/>
+                </div>
+                <div className="video__item">
+                    <video src={SD} controls/>
+                </div>
+            </div>
+        </div>
     )
 }
 
