9bbcde3d369511ab3c227aff30bd53d540ba5796
Gen <kebikovgen@gmail.com>
2022-12-09 13:29:19 +0300

    added toolkit in app and changed redux-store

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,9 @@
-import {createStore} from 'redux';
 import reducer from '../reducers/reducer';
+import { configureStore } from '@reduxjs/toolkit';
 
-const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = configureStore({
+    reducer: reducer,
+    devTools: process.env.NODE_ENV !== 'production'
+});
 
 export default store;
