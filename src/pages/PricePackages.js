342716a4204dfd1d59245332a135cb1461c2603b
Kebikov Evgenii <kebikovgen@gmail.com>
2023-06-07 23:16:51 +0300

    remade  index.html

--- src/pages/PricePackages.js
+++ src/pages/PricePackages.js
@@ -28,6 +28,7 @@ const PricePackages = () => {
                 <PricePackageHeader title={arrHeader[0].title} cash={arrHeader[0].cash} description={arrHeader[0].description} arrParagraphs={arrHeader[0].arrParagraphs} pic={arrHeader[0].pic}/>
                 <PricePackageHeader title={arrHeader[1].title} cash={arrHeader[1].cash} description={arrHeader[1].description} arrParagraphs={arrHeader[1].arrParagraphs} pic={arrHeader[1].pic}/>
                 <PricePackageHeader title={arrHeader[2].title} cash={arrHeader[2].cash} description={arrHeader[2].description} arrParagraphs={arrHeader[2].arrParagraphs} pic={arrHeader[2].pic}/>
+                <PricePackageHeader title={arrHeader[2].title} cash={arrHeader[2].cash} description={arrHeader[2].description} arrParagraphs={arrHeader[2].arrParagraphs} pic={arrHeader[2].pic}/>
                 <FormContact color={'#f1ece7'}/>
             </div>
     )
