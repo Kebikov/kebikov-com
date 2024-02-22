a8d05bf634705ea45cd596aced6a7213937e0f03
Kebikov Evgenii <kebikovgen@gmail.com>
2024-02-22 11:56:21 +0300

    added 'BUILD.md'

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -24,11 +24,22 @@ const Main = () => {
     return(
             <div className="container">
                 <Helmet>
-                    <title>Свадебный Фотограф Минск, Кебиков Евгений.</title>
+                
+                    <title>Фотографии свадеб в Минске,свадебные фотосессии в Минске.</title>
                     <meta
                         name="description"
-                        content="Яркие, живые, наполненные улыбками и радостью, фотографии вашего свадебного дня.Свадебный фотограф Минск,Кебиков Евгений +375 29 694-98-43"
+                        content="Фотографии свадеб снятых в Минске, свадебные фотосессии в Минске. Просмотрите съемки свадебных дней молодожен в Минске. Вам будет интересно !"
                     />
+                    <meta property="og:type" content="website"/>
+                    <meta property="og:url" content="https://kebikov.com/"/>
+                    <meta property="og:title" content="Свадебный Фотограф Минск, Кебиков Евгений" />
+                    <meta property="og:description" content="Яркие, живые, наполненные улыбками и радостью, фотографии вашего свадебного дня.Свадебный фотограф Минск,Кебиков Евгений +375 29 694-98-43"/>
+                    <meta property="og:image" content="https://kebikov.com/static/graph/kebikov.jpg"/>
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:image" content="https://kebikov.com/static/graph/kebikov.jpg" />
+                    <meta data-hid="property::og:site_name" property="og:site_name" content="KEBIKOV.COM"/>
+                    <link rel="canonical" href="https://kebikov.com/weddings-all" />
+
                     <link rel="canonical" href="https://kebikov.com/" />
                 </Helmet>
                 
@@ -40,3 +51,5 @@ const Main = () => {
 }
 
 export default Main;
+
+
--- src/pages/WeddingsAll.jsx
+++ src/pages/WeddingsAll.jsx
@@ -22,6 +22,14 @@ const WeddingsAll = () => {
                         name="description"
                         content="Фотографии свадеб снятых в Минске, свадебные фотосессии в Минске. Просмотрите съемки свадебных дней молодожен в Минске. Вам будет интересно !"
                     />
+                    <meta property="og:type" content="website"/>
+                    <meta property="og:url" content="https://kebikov.com/"/>
+                    <meta property="og:title" content="Свадебный Фотограф Минск, Кебиков Евгений" />
+                    <meta property="og:description" content="Яркие, живые, наполненные улыбками и радостью, фотографии вашего свадебного дня.Свадебный фотограф Минск,Кебиков Евгений +375 29 694-98-43"/>
+                    <meta property="og:image" content="https://kebikov.com/static/graph/kebikov.jpg"/>
+                    <meta name="twitter:card" content="summary_large_image" />
+                    <meta name="twitter:image" content="https://kebikov.com/static/graph/kebikov.jpg" />
+                    <meta data-hid="property::og:site_name" property="og:site_name" content="KEBIKOV.COM"/>
                     <link rel="canonical" href="https://kebikov.com/weddings-all" />
                 </Helmet>
 
